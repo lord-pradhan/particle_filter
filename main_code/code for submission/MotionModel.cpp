fa4f94e6fa91a906a9ced9318fca55319b54e9d4
Roshan <roshannpradhan@gmail.com>
2020-10-06 10:45:28 -0400

    final submitted version

---
+++ main_code/code for submission/MotionModel.cpp
@@ -0,0 +1,99 @@
+/*
+Odometry Motion Model from Probabilistic Robotics Chapter 5 
+*/
+
+class MotionModel{
+
+private:
+	
+	double alpha1;
+	double alpha2;
+	double alpha3;
+	double alpha4;
+
+	std::default_random_engine generator;
+
+public:
+
+	MotionModel(float a1, float a2, float a3, float a4)
+	{
+		// Error Parameters
+		alpha1 = a1;
+		alpha2 = a2;
+		alpha3 = a3;
+		alpha4 = a4;
+	}
+
+	double normaldist(double mean, double stddev)
+	{
+    	std::normal_distribution<double> distribution(mean,stddev);
+    	double sample = distribution(generator);
+		return sample;
+	}
+
+	odomMsg update(odomMsg u_t0, odomMsg u_t1, odomMsg x_t0)
+	{
+		double delta_rot1;
+		double delta_trans;
+		double delta_rot2;
+
+		double delta_rot1_hat;
+		double delta_trans_hat;
+		double delta_rot2_hat;
+
+		double x_p;
+		double y_p;
+		double theta_p;
+
+		double x_bar;
+		double y_bar;
+		double theta_bar;
+
+		double x_bar_p;
+		double y_bar_p;
+		double theta_bar_p;
+
+		double x;
+		double y;
+		double theta;
+
+		x_bar = u_t0.x;
+		y_bar = u_t0.y;
+		theta_bar = u_t0.theta;
+
+		x_bar_p = u_t1.x;
+		y_bar_p = u_t1.y;
+		theta_bar_p = u_t1.theta;
+
+		x = x_t0.x;
+		y = x_t0.y;
+		theta = x_t0.theta;
+
+		delta_rot1 = atan2(y_bar_p - y_bar, x_bar_p - x_bar) - theta_bar;
+		delta_trans = sqrt((x_bar_p - x_bar)*(x_bar_p - x_bar) + (y_bar_p - y_bar)*(y_bar_p - y_bar));
+		delta_rot2 = theta_bar_p - theta_bar - delta_rot1;
+
+		// cout << delta_trans << endl;
+
+		delta_rot1_hat = delta_rot1 - normaldist(0.0,sqrt(alpha1*pow(delta_rot1,2) + alpha2*pow(delta_trans,2)));
+		delta_trans_hat = delta_trans - normaldist(0.0,sqrt(alpha3*pow(delta_trans,2) + alpha4*pow(delta_rot1,2) + alpha4*pow(delta_rot2,2)));
+		delta_rot2_hat = delta_rot2 - normaldist(0.0,sqrt(alpha1*pow(delta_rot2,2) + alpha2*pow(delta_trans,2)));
+
+		x_p = x + delta_trans_hat*cos(theta+delta_rot1_hat);
+		y_p = y + delta_trans_hat*sin(theta+delta_rot1_hat);
+		theta_p = theta + delta_rot1_hat + delta_rot2_hat;
+
+		// cout << x << " " << x_p << endl;
+		// cout << y << " " << y_p << endl;
+		// cout << endl;
+
+		odomMsg x_t(x_p, y_p, theta_p);
+
+		return x_t;
+	}
+};
+
+// int main()
+// {
+// 	return 0;
+// }
---
+++ main_code/code for submission/utils.cpp
@@ -0,0 +1,59 @@
+#include <math.h>
+
+// helper functions
+void tileToAbs(int tileX, int tileY, double& absX, double& absY, int resolution){
+	// absX = tileX*resolution
+}
+
+void absToTile(int& tileX, int& tileY, double absX, double absY, int resolution){
+	tileX = floor((double) absX/resolution); tileY = floor((double)  absY/resolution);
+}
+
+bool inRangeMap(int tileX, int tileY, const vector<vector<int>>& mapIn){
+	bool returnVal = true;
+	int Ymax = mapIn.size(), Xmax = mapIn[0].size();
+	
+	if(tileX<=0 || tileX>=Xmax || tileY<=0 || tileY>=Ymax) returnVal = false;
+
+	return returnVal;
+}
+
+class odomMsg
+{
+public:
+	double x, y, theta;
+
+	odomMsg(){}
+
+	odomMsg(double x_init, double y_init, double theta_init)
+	{
+		x = x_init;
+		y = y_init;
+		theta = theta_init;
+	}
+};
+
+class wtOdomMsg
+{
+public:
+	double x, y, theta, wt;
+
+	wtOdomMsg(){}
+
+	wtOdomMsg(odomMsg odomIn, double wtIn){
+		x = odomIn.x; y = odomIn.y; theta = odomIn.theta; wt = wtIn;
+	}
+
+	wtOdomMsg(double x_init, double y_init, double theta_init, double wt_init){
+		x = x_init; y = y_init; theta = theta_init; wt = wt_init;
+	}
+};
+
+vector<vector<int>> transformoccupancymap(const vector<vector<int>>& map)
+{
+	vector<vector<int>> transformedmap(map.size(), vector<int>(map[0].size(), -1));	
+	for(int i = 0; i < map.size(); i++)
+		for(int j = 0; j < map[0].size(); j++)
+			transformedmap[i][j] = map[map.size()-1-i][j];
+	return transformedmap;
+}
